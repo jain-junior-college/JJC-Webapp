c70413a6f1cb7958dfa311e51fd8b68b17ae856b
JJ College Admin <admin@jaincollege.edu>
2026-04-16 09:27:16 +0530

    Increase password hash column length

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(20), default='staff') # admin or staff
 
 class Student(db.Model):
