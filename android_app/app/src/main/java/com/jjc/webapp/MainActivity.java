238bde59a49d64a04444e60ef9ade2f69003cc06
JJ College Admin <admin@jaincollege.edu>
2026-04-30 18:59:11 +0530

    Add Android mobile app project and GitHub Action for automated APK build

---
+++ android_app/app/src/main/java/com/jjc/webapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.jjc.webapp;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://jjc-webapp-1.onrender.com");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
